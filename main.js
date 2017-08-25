6c6444e54f9d601b130e0b3237f670891d24f0bc
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2017-08-25 07:58:46 +0200

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener('devicemotion', deviceMotionHandler, false);
+
+function deviceMotionHandler(eventData) {
+    
+    var acc = eventData.acceleration
+    var accg = eventData.accelerationIncludingGravity;
+    var gy = Math.abs(accg.y - acc.y);
+    var rgy = Math.floor(gy);
+
+    if (rgy < 9) {
+        alert("not straight");
+    }
+
+    document.getElementById('acc-readings').innerHTML = gy.toFixed(2);
+}
